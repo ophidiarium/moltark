7345ec0ae58a7ce2a622cfddae880ca42d0b38d0
Konstantin Vyatkin <tino@vtkn.io>
2026-04-03 13:49:46 +0200

    Add Composer stub package for Packagist name reservation

---
+++ src/Moltark.php
@@ -0,0 +1,7 @@
+<?php
+// moltark - Modern software project templater
+// https://github.com/ophidiarium/moltark
+
+namespace Ophidiarium\Moltark;
+
+throw new \RuntimeException('moltark is not yet published. See https://github.com/ophidiarium/moltark for status.');
