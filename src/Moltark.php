89bb272e7484c1bf1cfc64c4d1fee5499990627b
Konstantin Vyatkin <tino@vtkn.io>
2026-04-03 13:54:09 +0200

    Rename Composer package to moltark/moltark

--- src/Moltark.php
+++ src/Moltark.php
@@ -2,6 +2,6 @@
 // moltark - Modern software project templater
 // https://github.com/ophidiarium/moltark
 
-namespace Ophidiarium\Moltark;
+namespace Moltark\Moltark;
 
 throw new \RuntimeException('moltark is not yet published. See https://github.com/ophidiarium/moltark for status.');
